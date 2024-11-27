0972ba555f775b4953ed97328e2f28af07c3f125
Sujit Yalmar <sujityalmar02@gmail.com>
2024-11-27 11:25:57 +0530

    add file

---
+++ Restaurant.py
@@ -0,0 +1,10 @@
+import random
+import time
+import datetime
+import tkinter.messagebox
+from tkinter import *
+
+root=Tk()
+root.geometry("1350x750+0+0")
+root.title("Restaurant Management System")
+root.configure(background= 'Cadet Blue')
